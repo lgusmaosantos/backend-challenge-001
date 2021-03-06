9b1184dfd2ee839a13fe3ca64d03e81ec0546357
Luiz Gabriel Gusmao Santos <lgusmaosantos@gmail.com>
2021-03-06 17:43:32 -0300

    FIx missing `account` app URLs

--- settings/urls.py
+++ settings/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'health-check/$', health_check, name='health_check'),
 
     # Applications
+    url(r'^', include('accounts.urls')),
     url(r'^', include(main_router.urls)),
     url(r'^', include(posts_router.urls)),
     url(r'^', include(comments_router.urls))
